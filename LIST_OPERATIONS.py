d722e628d96b9f7bd2320aa9d5aa423465069bed
Assisiernest <assisiernest6@gmail.com>
2025-09-24 11:42:51 +0300

    Create LIST_OPERATIONS.py

---
+++ LIST_OPERATIONS.py
@@ -0,0 +1,11 @@
+list1 = [10,20,30,40]
+list2 = [50,60,70]
+
+#length 
+print("lenght of list1",len(list1))
+
+concatentated = list1 + list2
+print("concatentated list:",concatentated)
+
+print("first element of list 1:",list1[0])
+print("last element of list 2:",list2[-1])
